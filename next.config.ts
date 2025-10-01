9db77ec933af5dc955e1a181784089db390a7a96
ImStudent02 <imstudentasyc@gmail.com>
2025-10-01 21:32:55 +0530

    build update added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+// const nextConfig: NextConfig = {
   /* config options here */
+// };
+
+// export default nextConfig;
+
+const nextConfig = {
+  output: 'export',   // 👈 this makes Next.js put final site in
 };
 
 export default nextConfig;
+
