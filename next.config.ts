38bbec938a100c04f469f897f7e8c1b2c05392fc
ImStudent02 <imstudentasyc@gmail.com>
2025-10-03 16:45:22 +0530

    build ready project

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,12 @@
 import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-  /* config options here */
-// };
-
-// export default nextConfig;
-
-const nextConfig = {
-  output: 'export',   // 👈 this makes Next.js put final site in
-  ignoreDuringBuilds: true, // 👈 this disables ESLint during builds
+const nextConfig: NextConfig = {
+  //output: "export", // for static export
+  eslint: {
+    ignoreDuringBuilds: true, // ✅ proper way
+  },
 };
 
 export default nextConfig;
+
 
